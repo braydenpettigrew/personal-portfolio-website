1700a2826ba61f9ec134eb6824715916b945e2ea
Brayden Pettigrew <blp5473@psu.edu>
2024-03-17 20:50:53 -0400

    jQuery for tabs in about section

---
+++ JQ.js
@@ -0,0 +1,38 @@
+// $ is called a selecter and it is used to access an ID from the html document. (document.getElementByID)
+// $(element name 1, 2, 3, ...) == document.getElementByTagName(element name)
+// fadeToggle is a function from jquery library
+// class names start with "."
+// id names start with "#"
+// jquery selectors need unique id's
+// $("parent tag > child tag > another child tag")
+
+
+// $(document).ready(function(){
+//     // Fades an image in and out
+//     function func1(){
+//         $("#portrait").fadeToggle(3000);
+//     }
+
+//     function func2(){
+//         $("div").css('background-color','orange');
+//         $("#p1").css('font-style','italic');
+//     }
+
+//     function opentab(){
+//         $(".tab-links").removeClass("active-link");
+//         $(".tab-contents").removeClass("active-tab");
+
+//     }
+
+$(document).ready(function(){
+    // Attach click event listener to tabs
+    $(".tab-links").click(function() {
+        $(".tab-links").removeClass("active-link");
+        $(".tab-contents").removeClass("active-tab");
+        $(this).addClass("active-link");
+        // Get the data-id attribute value of the clicked tab link
+        var tabId = $(this).data("id");
+        // Add active class to the corresponding tab content
+        $("#" + tabId).addClass("active-tab");
+    });
+});
